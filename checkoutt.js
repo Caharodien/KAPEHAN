ddf3a0b02ab01405bbab75bc24da16ce5bf7b6e5
QuinntinChuy <james.espinosa39@gmail.com>
2025-04-06 22:10:21 +0800

    adding animations

--- checkoutt.js
+++ checkoutt.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // Update the view order list immediately
             updateViewOrderList(overviewData);
 
-            alert(`Receipt #${priorityNumber} confirmed!\n${orderNumber}\nTotal: ₱${totalPrice.toFixed(2)}`);
+            alert(`Priority #${priorityNumber} confirmed!\n${orderNumber}\nTotal: ₱${totalPrice.toFixed(2)}`);
 
             // Animation before redirect
             body.classList.add("pull-down-exit");
